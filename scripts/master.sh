3f7b7dae51f2567225fa696145cf7866a1883663
xbrouwer <xbrouwer@quintor.nl>
2017-02-03 12:16:23 +0100

    Add executable rights to getadmin script

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo ./default.sh
 wget https://raw.githubusercontent.com/xanvierb/UbuntuKubeadmInstall/master/scripts/getadminconf.sh
+chmod +x getadminconf.sh
 echo "------ $(date) ------" >> initlog.txt
 sudo kubeadm init >> initlog.txt
 sudo kubectl apply -f https://git.io/weave-kube
