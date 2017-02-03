0967c54aa7ea4f7b4f8f32e51386515303e5c162
xbrouwer <xbrouwer@quintor.nl>
2017-02-03 12:13:02 +0100

    Added possibillities for other user/machine names.

--- local/getadminonlocal.sh
+++ local/getadminonlocal.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # beware, there are a lot of stuff configured for my specific solution
 # like the username, password and machine names.
-# 
-sshpass -p 'board' ssh up@up01 /home/up/getadminconf.sh
-sshpass -p 'board' scp up@up01:/home/up/admin.conf $HOME/.kube/config
+#
+echo "Type the username that you want to check use (like up), followed by [ENTER]:" 
+read un
+echo "Type the machinename of the master"
+read mn
+echo $un@$mn will be used!
+read abc
+sshpass -p 'board' ssh $un@$mn /home/up/getadminconf.sh
+sshpass -p 'board' scp $un@$mn:/home/up/admin.conf $HOME/.kube/config
--- scripts/getadminconf.sh
+++ scripts/getadminconf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo board | sudo -S cp /etc/kubernetes/admin.conf ./
-sudo chown up:up admin.conf
+sudo chown $USER:$USER admin.conf
 chmod +rw admin.conf
